d42e4ae4da1ad41c127558694768284fcd36685e
KAMLESH KUMAR <kamlesh_kumar174@yahoo.com>
2025-02-07 22:15:54 +0500

    errors solved

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -36,9 +36,9 @@ export function Sidebar() {
         </ul>
       </nav>
       <div className="p-4">
-        <Button variant="ghost" className="w-full justify-start">
+        <Button variant="ghost" className="w-full justify-start bg-white text-black hover:bg-red-600 hover:text-white">
           <LogOut className="mr-2 h-5 w-5" />
-          Logout
+          <Link href='/login'>Logout</Link>
         </Button>
       </div>
     </div>
